0d11cbf0da3bf4bf6917aa04f597cf405ebeeb8f
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2024-02-15 11:51:30 +0300

    Log warnings when Cube is out of bounds

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -7,6 +7,8 @@ public class Cube : MonoBehaviour
   public Rigidbody2D myRigidBody;
   public float movementSpeed;
 
+  public float screenEdge;
+
   int numberOfTimes = 5;
   // Start is called before the first frame update
   void Start()
@@ -28,6 +30,12 @@ public class Cube : MonoBehaviour
     if (Input.GetKeyDown(KeyCode.RightArrow))
     {
       myRigidBody.velocity = new Vector2(movementSpeed, 0f);
+    }
+
+    float myPositionX = myRigidBody.position.x;
+    if (myPositionX > screenEdge || myPositionX < -screenEdge)
+    {
+      Debug.LogWarning($"Cube position out of bonds at position {myPositionX}");
     }
   }
 }
